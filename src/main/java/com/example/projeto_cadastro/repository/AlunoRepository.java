a33ab2a3886dc32edbf03e84ff1ebf173f2d479d
Pablo Ramos <pablohenrique2210@gmail.com>
2025-11-21 02:35:34 -0300

    Add files via upload

---
+++ src/main/java/com/example/projeto_cadastro/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package com.example.projeto_cadastro.repository;
+
+import com.example.projeto_cadastro.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+}
